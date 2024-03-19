dae64372977d478edc1016ad7fc8356b99b3a0a2
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-03-18 18:25:44 -0600

    add padding on small devices. move whtie btn out of @layer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://springshardwood.com',
-  integrations: [mdx(), sitemap(), icon({
-    include: {
-      tabler: ["*"]
-    }
-  }), tailwind()],
+  site: "https://springshardwood.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        tabler: ["*"],
+      },
+    }),
+    tailwind(),
+  ],
   output: "static",
-  adapter: netlify()
+  adapter: netlify(),
 });
