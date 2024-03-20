329bba64b2fd6d700fdf0df82ccef56518894e1a
Jesse Mutzebaugh <jesse@newvistadigital.com>
2024-03-20 13:06:17 -0600

    feat: add partytown and ga4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,29 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
-
 import robotsTxt from "astro-robots-txt";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://springshardwood.com",
-  integrations: [mdx(), sitemap(), icon({
-    include: {
-      tabler: ["*"]
-    }
-  }), tailwind(), robotsTxt()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        tabler: ["*"],
+      },
+    }),
+    tailwind(),
+    robotsTxt(),
+    partytown({
+      // Example: Add dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "static",
-  adapter: netlify()
+  adapter: netlify(),
 });
