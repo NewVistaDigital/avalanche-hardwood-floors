d7027808dc3a7c95246da4c016b9bd9642adebc4
Jesse Mutzebaugh <jesse@newvistadigital.com>
2024-03-27 12:51:43 -0600

    fix(html): wrap title tags in curly brackets to escape characters

--- src/consts.ts
+++ src/consts.ts
@@ -7,13 +7,13 @@
 import { slugify } from './js/slugify';
 
 export const SITE_TITLE = 'Avalanche Hardwood Floors';
-export const SITE_DESCRIPTION = 'The best hardwood floor company in Colorado Springs!';
+export const SITE_DESCRIPTION = 'The best hardwood floor company in Colorado Springs! Quality service you can trust. Call our owner for a free, no pressure estimate today.';
 export const SITE_URL = 'https://springshardwood.com/';
 export const SITE_EMAIL = 'rickmutz@yahoo.com';
-export const SITE_UTM_SOURCE = slugify(SITE_TITLE);
 export const SITE_DEFAULT_IMAGE = '/site-preview.png';
 export const SITE_PHONE = '(719) 492-8134'
 export const SITE_PHONE_HREF = 'tel:7194928134'
+export const SITE_UTM_SOURCE = slugify(SITE_TITLE);
 
 export const UI_THEME = 'system'; // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
 
